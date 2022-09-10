51f5cd77115eb0256a27e38a09d8af2e496df474
onishi taisei <taisei.developer@gmail.com>
2022-09-10 20:45:47 +0900

    現在地をobserveできるように実装

--- lib/presentasion/home/home_page.dart
+++ lib/presentasion/home/home_page.dart
@@ -7,39 +7,62 @@ import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../feature/auth/auth.dart';
 
-class HomePage extends HookConsumerWidget {
+class HomePage extends StatefulWidget {
   HomePage({super.key});
-  final _controller = Completer<dynamic>();
-
-  Future<void> getCurrentPosition() async {
-    final currentPosition = await Geolocator.getCurrentPosition();
-  }
-
-  static const CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
-
-  static const CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _controller = Completer<dynamic>();
+
+  Position? currentPosition;
+
+  late StreamSubscription<Position> positionStream;
+
+  //初期位置
+  final CameraPosition _kGooglePlex = const CameraPosition(
+    target: LatLng(43.0686606, 141.3485613),
+    zoom: 14,
+  );
+
+  final LocationSettings locationSettings = const LocationSettings(
+    accuracy: LocationAccuracy.high, //正確性:highはAndroid(0-100m),iOS(10m)
+    distanceFilter: 100,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    //位置情報が許可されていない時に許可をリクエストする
+    Future(() async {
+      LocationPermission permission = await Geolocator.checkPermission();
+      if (permission == LocationPermission.denied) {
+        await Geolocator.requestPermission();
+      }
+    });
+
+    //現在位置を更新し続ける
+    positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position? position) {
+      currentPosition = position;
+      print(position == null
+          ? 'Unknown'
+          : '${position.latitude.toString()}, ${position.longitude.toString()}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(onPressed: () {
-        ref.read(signOutProvider).call();
-      }),
       body: GoogleMap(
-        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex, // デフォルトのカメラ位置
         myLocationButtonEnabled: false, // デフォルトの現在地ボタン
         myLocationEnabled: true, // 現在地アイコンの表示
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+        onMapCreated: _controller.complete,
       ),
     );
   }
